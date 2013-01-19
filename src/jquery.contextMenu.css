46b5317a41e7504e20e599f721c2253949be70e8
Rodney Rehm <mail+github@rodneyrehm.de>
2013-01-19 21:42:53 +0100

    version bump

--- src/jquery.contextMenu.css
+++ src/jquery.contextMenu.css
@@ -1,7 +1,7 @@
 /*!
  * jQuery contextMenu - Plugin for simple contextMenu handling
  *
- * Version: 1.6.2
+ * Version: 1.6.3
  *
  * Authors: Rodney Rehm, Addy Osmani (patches for FF)
  * Web: http://medialize.github.com/jQuery-contextMenu/
