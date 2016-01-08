491346ceb2aea19bf25212683a0b623a499341b4
Bjorn Brala <bjorn@swis.nl>
2016-01-08 19:48:45 +0100

    Update test paths to src.

--- gulpfile.js
+++ gulpfile.js
@@ -159,9 +159,9 @@ gulp.task('build-icons', function () {
  */
 gulp.task('integration-test-paths', function(){
     return gulp.src('test/integration/html/*.html').
-    pipe(plugins.replace('http\:\/\/swisnl\.github\.io\/jQuery\-contextMenu\/dist\/', '\.\.\/\.\.\/\.\.\/dist\/')).
-    pipe(plugins.replace('http\:\/\/swisnl\.github\.io\/jQuery\-contextMenu\/dist\/', '\.\.\/\.\.\/\.\.\/dist\/')).
-    pipe(plugins.replace('http\:\/\/swisnl\.github\.io\/jQuery-contextMenu\/js\/jquery\.ui\.position\.min\.js', '\.\.\/\.\.\/\.\.\/dist\/jquery\.ui\.position\.min\.js')).
+    pipe(plugins.replace('http\:\/\/swisnl\.github\.io\/jQuery\-contextMenu\/dist\/', '\.\.\/\.\.\/\.\.\/src\/')).
+    pipe(plugins.replace('http\:\/\/swisnl\.github\.io\/jQuery\-contextMenu\/dist\/', '\.\.\/\.\.\/\.\.\/src\/')).
+    pipe(plugins.replace('http\:\/\/swisnl\.github\.io\/jQuery-contextMenu\/js\/jquery\.ui\.position\.min\.js', '\.\.\/\.\.\/\.\.\/src\/jquery\.ui\.position\.min\.js')).
     pipe(gulp.dest('test/integration/html/'));
 });
 
