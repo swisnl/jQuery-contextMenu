6c112709a88753da925180a4ce07a9187647a03f
Bjorn Brala <bjorn@swis.nl>
2016-02-28 09:39:30 +0100

    Reverted 616b2496710fc9227a5cd0f65c05f5ff051f5bf7, gives some problems when navigating with keyboard. Reopens #346

--- src/jquery.contextMenu.js
+++ src/jquery.contextMenu.js
@@ -800,9 +800,7 @@
 
                 // make sure only one item is selected
                 (opt.$menu ? opt : root).$menu
-                    .children(root.classNames.hover).trigger('contextmenu:blur');
-                // Also check this for all siblings of the LI
-                $this.siblings().trigger('contextmenu:blur');
+                    .children('.hover').trigger('contextmenu:blur');
 
                 if ($this.hasClass(root.classNames.disabled) || $this.hasClass(root.classNames.notSelectable)) {
                     opt.$selected = null;
