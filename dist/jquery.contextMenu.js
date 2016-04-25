3feea5bea1cac0986f0e46c640229e0beb607a7c
Rogier Mechielsen <rogier@swis.nl>
2016-04-25 12:27:13 +0200

    made all units relative with ems

--- dist/jquery.contextMenu.js
+++ dist/jquery.contextMenu.js
@@ -12,7 +12,7 @@
  *   MIT License http://www.opensource.org/licenses/mit-license
  *   GPL v3 http://opensource.org/licenses/GPL-3.0
  *
- * Date: 2016-04-08T15:05:16.920Z
+ * Date: 2016-04-25T09:49:48.643Z
  */
 
 (function (factory) {
