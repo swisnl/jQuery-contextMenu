0e74b086c47728cdf49488bb6d474b0e08eb8a0a
Sweet-Bob <dampilon.robert@gmail.com>
2016-10-25 20:00:28 +0700

    Add check on destroy (Menu can be empty)
    In my app, sometimes menu is empty (i.e. [o param is empty](https://github.com/swisnl/jQuery-contextMenu/blob/master/src/jquery.contextMenu.js#L1658)), i think this condition it will not be superfluous

--- src/jquery.contextMenu.js
+++ src/jquery.contextMenu.js
@@ -1592,6 +1592,10 @@
                     // get proper options
                     var context = o.context;
                     $.each(menus, function (ns, o) {
+                        
+                        if (!o) {
+                            return true;
+                        }
 
                         // Is this menu equest to the context called from
                         if (!$(context).is(o.selector)) {
